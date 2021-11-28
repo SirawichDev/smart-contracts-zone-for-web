6aa42586130e5f96934178bae2f3aeed46ed4916
SirawichDev <sirawit0676@gmail.com>
2021-11-28 20:38:42 +0700

    SCZ-000: make life easier with truffle XD

---
+++ truffle-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  },
+};
