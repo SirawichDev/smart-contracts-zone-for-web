11f0f1c23579c41e37d41ba17f94968ed7e7c8bb
SirawichDev <sirawit0676@gmail.com>
2021-12-26 11:28:30 +0700

    SCZ-000: add funds function and getFunder buy index,get funders

---
+++ contracts/SczContract.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract SCZ {
+    address[] public funders;
+
+    receive() external payable {}
+
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
+
+    function getAllFunders() public view returns (address[] memory) {
+        return funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns(address) {
+        address[] memory _funders = getAllFunders();
+        return _funders[index];
+    }
+}
